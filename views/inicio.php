f9d89dba0422850f52294a5ee233a0e707160683
Ismael-Cristino <ismcridav@alu.edu.gva.es>
2025-01-28 18:57:23 +0100

    Subiendo menu (v2) y arreglando head

--- views/inicio.php
+++ views/inicio.php
@@ -18,10 +18,8 @@
     <table class="table table-light table-hover">
       <thead class="table-dark">
         <tr>
-          <th scope="col"><a class="btn btn-primary" href="index.php?tabla=digimones&accion=ver&id=<?= $_SESSION["usuario"]->id ?>"><i class="fa-regular fa-eye"></i> Ver mis Digimons</a></th>
-          <th scope="col"><a class="btn btn-primary" href="index.php?tabla=digimones&accion=ver&id=<?= $_SESSION["usuario"]->id ?>"><i class="fa-solid fa-sitemap"></i> Organizar Equipo</a></th>
-          <th scope="col"><a class="btn btn-primary" href="index.php?tabla=digimones&accion=jugar&id=<?= $_SESSION["usuario"]->id ?>"><i class="fa-solid fa-hand-fist"></i> Jugar Partida</a></th>
-          <th scope="col"><a class="btn btn-primary" href="index.php?tabla=digimones&accion=evoluvcionar&id=<?= $_SESSION["usuario"]->id ?>"><i class="fa-solid fa-dna"></i> Digievolucionar</a></th>
+          <th scope="col"><a class="btn btn-primary" href="index.php?tabla=usuarios&accion=inventario&id=<?= $_SESSION["usuario"]->id ?>"><i class="fa-regular fa-eye"></i> Ver mis Digimons (Inventario)</a></th>
+          <th scope="col"><a class="btn btn-primary" href="index.php?tabla=usuarios&accion=combate&id=<?= $_SESSION["usuario"]->id ?>"><i class="fa-solid fa-hand-fist"></i> Jugar Partida</a></th>
         </tr>
       </thead>
     </table>
